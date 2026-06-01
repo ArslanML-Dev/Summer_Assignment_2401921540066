9ab7fb39725b2718c89259e4bff2a5e31554b3ea
ArslanML-Dev <arslankhandev84@gmail.com>
2026-06-01 22:17:19 +0530

    adding file 3

---
+++ Week_1/Day_1/three_sum.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution2 {
+    public int[] twoSum(int nums[], int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int findMe = target - nums[i];
+            if (map.containsKey(findMe)) {
+                return new int[] { map.get(findMe), i };
+            }
+            map.put(nums[i], i);// insert values
+
+        }
+        return null;
+    }
+}
