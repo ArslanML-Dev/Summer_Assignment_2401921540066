5a1193ac8eb77d0bf33a14231490a74c83feb060
ArslanML-Dev <arslankhandev84@gmail.com>
2026-06-01 22:31:53 +0530

    changed variable to 5

--- Week_1/Day_1/three_sum.java
+++ Week_1/Day_1/three_sum.java
@@ -5,7 +5,7 @@ class Solution2 {
     public int[] twoSum(int nums[], int target) {
         Map<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            int findMe = target - nums[i + 1 + 1];
+            int findMe = target - nums[i + 1 + 5];
             if (map.containsKey(findMe)) {
                 return new int[] { map.get(findMe), i };
             }
