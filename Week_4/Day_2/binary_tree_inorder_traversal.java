f86df1d05f23edb8c611a9cc0a6e85d21f7243dd
ARSLAN KHAN <arslankhandev84@gmail.com>
2026-06-23 22:15:25 +0530

    Implement inorder traversal for binary tree

---
+++ Week_4/Day_2/binary_tree_inorder_traversal.java
@@ -0,0 +1,17 @@
+class Solution {
+    void dfs(TreeNode root, List<Integer> res)
+    {
+        if(root==null)
+        return;
+        //l+r
+        dfs(root.left,res);
+        res.add(root.val);
+        dfs(root.right,res);
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+
+        List<Integer> res= new ArrayList<>();
+        dfs(root,res);
+        return res;        
+    }
+}
